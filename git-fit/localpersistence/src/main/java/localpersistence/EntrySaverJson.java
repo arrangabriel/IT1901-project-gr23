ae67911ea8dff88c69f68117843e88fc73c194f9
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2021-09-20 17:27:30 +0200

    Add beginning of a possible solution for load

--- git-fit/localpersistence/src/main/java/localpersistence/EntrySaverJson.java
+++ git-fit/localpersistence/src/main/java/localpersistence/EntrySaverJson.java
@@ -1,10 +1,16 @@
 package localpersistence;
 
 import java.util.HashMap;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.time.Duration;
 
+import core.EntryManager;
 import core.LogEntry;
 
 import org.json.simple.JSONObject;
@@ -14,17 +20,17 @@ import org.json.simple.parser.ParseException;
 public class EntrySaverJson {
 
     
-    private void save(HashMap<String, LogEntry> logEntry) throws IOException {
+    public void save(HashMap<String, LogEntry> entryManager) throws IOException {
 
         JSONObject json = new JSONObject();
         
 
-        for (String key : logEntry.keySet()){
+        for (String key : entryManager.keySet()){
             HashMap<String, String> innerMap = new HashMap<String, String>();
-            innerMap.put("title", logEntry.get(key).getTitle());
-            innerMap.put("comment", logEntry.get(key).getComment());
-            innerMap.put("date", logEntry.get(key).getDate().toString());
-            innerMap.put("duration", String.valueOf(logEntry.get(key).getDuartion().getSeconds()));
+            innerMap.put("title", entryManager.get(key).getTitle());
+            innerMap.put("comment", entryManager.get(key).getComment());
+            innerMap.put("date", entryManager.get(key).getDate().toString());
+            innerMap.put("duration", String.valueOf(entryManager.get(key).getDuartion().getSeconds()));
 
             json.put(key, innerMap);
         }
@@ -32,8 +38,66 @@ public class EntrySaverJson {
         file.createNewFile();
         FileWriter writer = new FileWriter(file);
         writer.write(json.toJSONString());
+        writer.flush();
+
         writer.close();
         }
+    
+    public String load() throws FileNotFoundException{
+        
+        JSONParser jsonParser = new JSONParser();
+        File file = new File("SavedData.json");
+        Scanner reader = new Scanner(file);
+        String dataString = "";
+
+        while (reader.hasNextLine()){
+            dataString += reader.hasNextLine();
+        }
+        reader.close();
+
+        try{
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(dataString);
+            EntryManager entryManager = new EntryManager(); 
+
+            for (Object key : jsonObject.keySet()){
+                String id = (String) key;
+                
+                //Suppressed unchecked warning. Any better solution Stefan?:
+                @SuppressWarnings("unchecked")
+                HashMap<String, String> innerMap = (HashMap<String, String>) jsonObject.get(key);
+                
+                String title;
+                String comment;
+                LocalDate date;
+                Duration duration;
+
+                for(String innerKey : innerMap.keySet()){
+                    if (innerKey.equals("title")){
+                        title = innerKey;
+                    }
+                    if (innerKey.equals("comment")){
+                        title = innerKey;
+                    }
+                    if (innerKey.equals("date")){
+                        title = innerKey;
+                    }
+                    if (innerKey.equals("duration")){
+                        title = innerKey;
+                    }
+                }
+            
+
+            }
+
+        }
+        catch (ParseException pException){
+            return null;
+        }
+
+        return null;
+
+    }
+    
 
     
 }
