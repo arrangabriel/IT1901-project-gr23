1de87b998bdc453453a9cf3ec706e93425579c51
Emilie Skirdal Frøhaug <emilieskirdalfrohaug@dhcp-10-22-2-105.wlan.ntnu.no>
2021-09-21 13:15:12 +0200

    Add change scene function

--- git-fit/ui/src/main/java/ui/AddNewSessionController.java
+++ git-fit/ui/src/main/java/ui/AddNewSessionController.java
@@ -6,9 +6,22 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import core.EntryManager;
+
+
 
 public class AddNewSessionController {
-    
+    private EntryManager entryManager;
+
     //fxml component attributes
     @FXML private Label header;
     @FXML private Label timeLabel;
@@ -17,13 +30,49 @@ public class AddNewSessionController {
     @FXML private Label commentLabel;
 
     @FXML private TextField nameOfSessionField;
-    @FXML private TextField timeField;
     @FXML private TextField distanceField;
+    @FXML private TextField hour;
+    @FXML private TextField min;
     @FXML private TextArea commentField;
 
     @FXML private DatePicker sessionDatePicker;
 
     @FXML private Button createSession;
+
+    /*private void timeValueValidation() implements ChangeListener<String> {
+        @Override
+        public void changed(ObservebleValue<? extends String>) observeble, String OldValue, String newValue){
+            try {
+                 Integer.parseInt(observeble.GetValue()))
+            }
+            catch (NumberFormatException e){
+
+            }
+
+            }
+            
+        }
+
+    }*/
+
+
+
+    @FXML 
+    public void createSessionButtonPushed(ActionEvent event) throws IOException{
+        //entryManager = new EntryManager();
+        //entryManager.addEntry(nameOfSessionField.getText(),commentField.getText(),sessionDatePicker.getValue(), Duration.ofSeconds(1));
+
+        //logEntry.setTitle(nameOfSessionField.getText());
+        //logEntry.setComment(commentField.getText());
+        //logEntry.setDate(sessionDatePicker.GetValue());
+        //Integer.parseInt(start_h.getText)*60
+        //logEntry.setDuration()
+        App.setRoot("StartPage");
+
+    }
+
+  
+
 
     @FXML
     private void initialize(){
