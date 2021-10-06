ca354fe153d950e461268e3152184541ccb0b82a
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-10-06 20:00:08 +0200

    Add math package

---
+++ get-fit/core/src/main/java/math/Statistics.java
@@ -0,0 +1,11 @@
+package math;
+
+import core.EntryManager;
+
+public class Statistics {
+    
+    public static int getCount(EntryManager entryManager){
+        return entryManager.entryCount();
+    }
+
+}
