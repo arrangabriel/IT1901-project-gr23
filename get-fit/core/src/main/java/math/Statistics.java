8059bd14e42d4c9bf7ee7b0f7beb5494b872ef5c
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-10-07 11:08:56 +0200

    Add average calculations

--- get-fit/core/src/main/java/math/Statistics.java
+++ get-fit/core/src/main/java/math/Statistics.java
@@ -1,11 +1,27 @@
 package math;
 
 import core.EntryManager;
+import core.LogEntry;
 
 public class Statistics {
     
     public static int getCount(EntryManager entryManager){
         return entryManager.entryCount();
+    }
+
+    /**
+     * Returns the average duration across all LogEntries in the EntryManager.
+     * @param entryManager the entryManager to average.
+     * @return the average.
+     */
+    public static double getAverageDuration(EntryManager entryManager) {
+        double sum = 0;
+        for (LogEntry logEntry : entryManager) {
+            sum += logEntry.getDuration().toSeconds();
+        }
+        double average = sum/entryManager.entryCount();
+
+        return average;
     }
 
 }
