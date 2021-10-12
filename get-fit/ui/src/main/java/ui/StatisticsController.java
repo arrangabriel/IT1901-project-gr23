567409af4bff50295e03edeba2420e78c75b8877
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2021-10-12 12:07:57 +0200

    #46 Change scene between home and stats

--- get-fit/ui/src/main/java/ui/StatisticsController.java
+++ get-fit/ui/src/main/java/ui/StatisticsController.java
@@ -2,9 +2,14 @@ package ui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.scene.control.ToggleGroup;
 
 
 import core.LogEntry;
@@ -19,17 +24,39 @@ public class StatisticsController {
     @FXML private Label maximumHr;
     @FXML private Label averageSpeed;
     @FXML private Label speed;
-    @FXML private ComboBox<String> timeInterval;
-    @FXML private ComboBox<String> exerciseType, tags;
+    @FXML private Button returnToStartPage;
+    @FXML private ComboBox<String> exerciseType;
+    @FXML private RadioButton weeks;
+    @FXML private RadioButton months;
+    @FXML private RadioButton years;
 
-    private ObservableList<String> exerciseTypeSelecter = FXCollections.observableArrayList("Running" , "Cycling", "Strength" ,"Svimming");
-    private ObservableList<String> timeIntervalSelector = FXCollections.observableArrayList("Year", "Month", "Week");
+
+    private ObservableList<String> exerciseTypeSelecter = FXCollections.observableArrayList("Cardio", "Endurance", "Not selected");
+    private final ToggleGroup toggleGroup = new ToggleGroup();
+    
+    //Stacked bar chart
+
 
     @FXML
     private void initialize() {
-        exerciseType.setItems(exerciseTypeSelecter);
-        timeInterval.setItems(timeIntervalSelector);   
+        exerciseType.setItems(exerciseTypeSelecter); 
+        weeks.setToggleGroup(toggleGroup);  
+        months.setToggleGroup(toggleGroup);
+        years.setToggleGroup(toggleGroup);
     }
 
+    
+    /**
+     * Switches the view to AddNewSession.
+     * @param event event data from pushed button.
+     * @throws IOException if .FXML file could not be found.
+     */
+   @FXML
+    public void returnToStartPagePushed(ActionEvent event) throws IOException{
+        System.out.println("hei");
+        App.setRoot("StartPage");
+    }
+
+    
     
 }
