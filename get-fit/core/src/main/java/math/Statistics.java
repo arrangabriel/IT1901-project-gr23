a5b465e1665709e41d9178fb8a30c4ed100f9678
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-10-12 11:52:26 +0200

    Add method getTotalDuration in Statistics class

--- get-fit/core/src/main/java/math/Statistics.java
+++ get-fit/core/src/main/java/math/Statistics.java
@@ -10,18 +10,28 @@ public class Statistics {
     }
 
     /**
+     * Returns the total duration of all LogEntries in the EntryManager.
+     * @param entryManager the entryManager to total.
+     * @return the total.
+     */
+    public static double getTotalDuration(EntryManager entryManager) {
+        double sum = 0;
+        for (LogEntry logEntry : entryManager) {
+            sum += logEntry.getDuration().toSeconds();
+        }
+
+        return sum;
+    }
+
+    /**
      * Returns the average duration across all LogEntries in the EntryManager.
      * @param entryManager the entryManager to average.
      * @return the average.
      */
     public static double getAverageDuration(EntryManager entryManager) {
-        double sum = 0;
-        for (LogEntry logEntry : entryManager) {
-            sum += logEntry.getDuration().toSeconds();
-        }
+        double sum = getTotalDuration(entryManager);
         double average = sum/entryManager.entryCount();
 
         return average;
     }
-
 }
