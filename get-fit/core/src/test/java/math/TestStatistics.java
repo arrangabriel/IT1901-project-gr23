a82a5fc2797b916352de9bad75625d70ca328c46
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2021-10-14 13:51:46 +0200

    #50 Create statistic test

--- get-fit/core/src/test/java/math/TestStatistics.java
+++ get-fit/core/src/test/java/math/TestStatistics.java
@@ -8,12 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import core.EntryManager;
 import core.LogEntry;
-import core.LogEntry.CARDIOSUBCATEGORIES;
-import core.LogEntry.EXERCISECATEGORY;
-import core.LogEntry.STRENGTHSUBCATEGORIES;
-import core.LogEntry.Subcategory;
 import core.LogEntry.EntryBuilder;
-import math.Statistics;
 
 public class TestStatistics {
 
@@ -24,7 +19,7 @@ public class TestStatistics {
         return new EntryManager();
     }
 
-    private EntryBuilder genValidEntryBuilder(String id, Duration duration, double distance, LogEntry.EXERCISECATEGORY exerciseCategory) {
+    private EntryBuilder genValidEntryBuilder(Duration duration, Double distance, LogEntry.EXERCISECATEGORY exerciseCategory) {
         String title = "Test";
         String comment = "This is a test";
         LocalDate date = LocalDate.now().minusDays(1);
@@ -40,13 +35,56 @@ public class TestStatistics {
     @Test
     public void testGetCount() {
         EntryManager manager = genValidEntryManager();
-        manager.addEntry(genValidEntryBuilder("1", Duration.ofSeconds(hour), 10, LogEntry.EXERCISECATEGORY.RUNNING));
-        manager.addEntry(genValidEntryBuilder("2", Duration.ofSeconds(hour), 10, LogEntry.EXERCISECATEGORY.RUNNING));
+        EntryBuilder builder = genValidEntryBuilder(Duration.ofSeconds(hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        String id = manager.addEntry(builder.build());
+        Assertions.assertEquals(1, Statistics.getCount(manager));
+        manager.addEntry(builder.build());
         Assertions.assertEquals(2, Statistics.getCount(manager));
+        manager.removeEntry(id);
+        Assertions.assertEquals(1, Statistics.getCount(manager));
     }
 
     @Test
     public void testGetTotalDuration() {
+        EntryManager manager = genValidEntryManager();
+        EntryBuilder builder1 = genValidEntryBuilder(Duration.ofSeconds(hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        EntryBuilder builder2 = genValidEntryBuilder(Duration.ofSeconds(2 * hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        manager.addEntry(builder1.build());
+        manager.addEntry(builder2.build());
+        //Compares with 3 hours which equals 10800 seconds. 
+        Assertions.assertEquals(10800, Statistics.getTotalDuration(manager));
+    }
+
+    @Test
+    public void testGetAverageDuration() {
+        EntryManager manager = genValidEntryManager();
+        EntryBuilder builder1 = genValidEntryBuilder(Duration.ofSeconds(hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        EntryBuilder builder2 = genValidEntryBuilder(Duration.ofSeconds(2 * hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        manager.addEntry(builder1.build());
+        manager.addEntry(builder2.build());
+        //Compares with 1.5 hours which is equal to  
+        Assertions.assertEquals(5400, Statistics.getAverageDuration(manager));
+    }
+
+    @Test
+    public void testGetAverageSpeed() {
+        EntryManager manager = genValidEntryManager();
+        EntryBuilder builder1 = genValidEntryBuilder(Duration.ofSeconds(hour), 5.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        EntryBuilder builder2 = genValidEntryBuilder(Duration.ofSeconds(2 * hour), 10.0, LogEntry.EXERCISECATEGORY.RUNNING);
+        EntryBuilder builder3 = genValidEntryBuilder(Duration.ofSeconds(3 * hour), 15.0, LogEntry.EXERCISECATEGORY.STRENGTH);
+        manager.addEntry(builder1.build());
+        manager.addEntry(builder2.build());
+        manager.addEntry(builder3.build());
+
+        Assertions.assertEquals(12, Statistics.getAverageSpeed(manager));
+    }
+
+    @Test
+    public void testIllegalGetAverageSpeed() {
+        EntryManager manager = genValidEntryManager();
+        EntryBuilder builder = genValidEntryBuilder(Duration.ofSeconds(hour), null, LogEntry.EXERCISECATEGORY.RUNNING);
+        manager.addEntry(builder.build());
+        Assertions.assertThrows(IllegalStateException.class, () -> Statistics.getAverageSpeed(manager));
     }
 
 
