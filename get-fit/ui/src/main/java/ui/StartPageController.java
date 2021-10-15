62bf412ba81b11c218efbabe742c95e51005d896
Arran Gabriel <arrankg@gmail.com>
2021-10-15 11:04:48 +0200

    Create skeleton for frontend sorting
    Signed-off-by: Arran Gabriel <arrankg@gmail.com>

--- get-fit/ui/src/main/java/ui/StartPageController.java
+++ get-fit/ui/src/main/java/ui/StartPageController.java
@@ -1,67 +1,143 @@
 package ui;
 
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import javafx.fxml.FXML;
+import core.EntryManager;
+import core.LogEntry;
 import javafx.event.ActionEvent;
-import javafx.fxml.Initializable;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.geometry.HPos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.stage.Stage;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import localpersistence.EntrySaverJson;
 
-import core.LogEntry;
-import core.EntryManager;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class StartPageController {
-    @FXML Button addSession;
-    @FXML ListView<String> listOfEntries;
-    @FXML Label errorLabel;
+
+    /***/
+    @FXML
+    private ComboBox<String> sortConfig;
+    /***/
+    @FXML
+    private ComboBox<String> sortCategory;
+    /***/
+    @FXML
+    private ComboBox<String> sortSubcategory;
+    /***/
+    @FXML
+    private Button sortReverse;
+    /***/
+    @FXML
+    private Button delete;
+    /***/
+    @FXML
+    private Button goToStatistics;
+    /***/
+    @FXML
+    private Button addSession;
+    /***/
+    @FXML
+    private ListView<VBox> listOfEntries;
+    /***/
+    @FXML
+    private Label errorLabel;
 
     /**
      * Switches the view to AddNewSession.
+     *
      * @param event event data from pushed button.
      * @throws IOException if .FXML file could not be found.
      */
     @FXML
-    public void addSessionButtonPushed(ActionEvent event) throws IOException{
+    public void addSessionButtonPushed(final ActionEvent event)
+            throws IOException {
         App.setRoot("AddNewSession");
     }
 
-    public void handleViewStatisticsButton(ActionEvent event) throws IOException{
-        App.setRoot("Statistics");
-    } 
     @FXML
-    public void deleteButtonPushed(ActionEvent event) throws IOException{
-        //App.entryManager.removeEntry(listOfEntries.getSelectionModel().getSelectedItem().getId());
-        //App.entryManager.removeEntry(listOfEntries.getSelectionModel().getSelectedItem().getId());
-      
+    public void handleGoToStatistics(final ActionEvent event)
+            throws IOException {
+        // TODO - change this to new version @Emilie
+        App.setRoot("Statistics");
     }
 
-    /**
-     * Iterates over the EntryManager of the app and adds the titles to listOfEntries.
-     */
-    public void addToList(){
-        for (LogEntry entry : App.entryManager){
-            listOfEntries.getItems().add(entry.getTitle());
+    @FXML
+    public void deleteButtonPushed(ActionEvent event) throws IOException {
+        //App.entryManager.removeEntry(listOfEntries.getSelectionModel().getSelectedItem().getId());
+    }
+
+    @FXML
+    public void sort(Event event) {
+        System.out.println("Whoopty");
+    }
+
+    public void addIteratorToView(final Iterator<LogEntry> entries) {
+        while (entries.hasNext()) {
+            LogEntry entry = entries.next();
+            listOfEntries.getItems().add(createListEntry(entry));
         }
     }
 
+    private VBox createListEntry(final LogEntry entry) {
+        VBox vBox = new VBox();
+        GridPane grid = new GridPane();
+
+        ColumnConstraints colConstraint = new ColumnConstraints();
+        colConstraint.setPercentWidth(25);
+        grid.getColumnConstraints()
+                .addAll(colConstraint, colConstraint, colConstraint,
+                        colConstraint);
+
+        Text title = new Text(entry.getTitle());
+        Text date = new Text(entry.getDate().toString());
+        Text category = new Text(entry.getExerciseCategory().toString());
+
+        // this is a placeholder.
+        Button open = new Button();
+        open.setText("show");
+
+        GridPane.setHalignment(title, HPos.LEFT);
+        GridPane.setHalignment(date, HPos.LEFT);
+        GridPane.setHalignment(category, HPos.LEFT);
+        GridPane.setHalignment(open, HPos.RIGHT);
+
+        grid.add(title, 0, 0);
+        grid.add(date, 1, 0);
+        grid.add(category, 2, 0);
+        grid.add(open, 3, 0);
+
+        vBox.getChildren().add(grid);
+
+        return vBox;
+    }
+
     /**
-     * Initializes the controller
+     * Initializes the controller.
      */
     @FXML
-    private void initialize(){
-        if(App.entryManager.entryCount()== 0){
-            try{
+    private void initialize() {
+        //populate sorting selectors
+
+
+        if (App.entryManager.entryCount() == 0) {
+            try {
                 EntrySaverJson.load(App.entryManager);
-            }
-            catch (FileNotFoundException e){
+            } catch (FileNotFoundException e) {
                 System.out.println("SaveData.json could not be found.");
                 errorLabel.setText("The file was not found.");
             }
         }
-        this.addToList();
+        Iterator<LogEntry> entries =
+                new EntryManager.SortedIteratorBuilder(App.entryManager,
+                        LogEntry.SORTCONFIGURATIONS.DATE).iterator(false);
+        this.addIteratorToView(entries);
     }
 }
