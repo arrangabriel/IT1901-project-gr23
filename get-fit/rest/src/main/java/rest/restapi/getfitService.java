57f090508889e92074f0498c743b79e0585b2e8a
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2021-10-27 11:20:14 +0200

    #78 Create rest module

---
+++ get-fit/rest/src/main/java/rest/restapi/getfitService.java
@@ -0,0 +1,10 @@
+package get-fit.restapi;
+
+@Path(getfitService.GETFIT_SERVICE_PATH)
+@Produces(MedaType.APPLICATION_JSON)
+
+public class getfitService {
+
+    private static final Logger logger = LoggerFactory.getLogger(getfitService.class);
+
+}
