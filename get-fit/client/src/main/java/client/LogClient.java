2b6ce84b1c4d43d6f6801f76f251e396ba287ee3
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-11-02 10:59:45 +0000

    #78 Add doc to functions

--- get-fit/client/src/main/java/client/LogClient.java
+++ get-fit/client/src/main/java/client/LogClient.java
@@ -21,10 +21,16 @@ import org.json.JSONObject;
  */
 public class LogClient{
 
+    /** URL to remote server. */
     private final String url;
     
+    /** Port to the remote server.  */
     private final int port;
 
+    /**
+     * Constructs a LogClient from a builder.
+     * @param builder
+     */
     public LogClient(LogClientBuilder builder) {
 
         this.url = builder.url;
@@ -32,6 +38,15 @@ public class LogClient{
 
     }
 
+    /**
+     * Gets a LogEntry from the server.
+     * @param id The id of the LogEntry to retrieve.
+     * @return The LogEntry represented by a hash map.
+     * @throws URISyntaxException If the id ruins the URI syntax.
+     * @throws IOException If the underlying resources cannot be allocated.
+     * @throws InterruptedException If the request was interrupted before retreiving the http response.
+     * @throws ExecutionException If the request completed exceptionally.
+     */
     public HashMap<String, String> getLogEntry(String id) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
 
         HttpResponse<String> response = this.get("/api/v1/entries/"+id);
@@ -49,10 +64,27 @@ public class LogClient{
 
     }
 
+    /**
+     * Get a list of log entries from the server without sorting or filtering.
+     * @return A list of log entries from the server represented as a hash map.
+     * @throws URISyntaxException If the hardcoded URI no longer matches the servers expectations.
+     * @throws IOException If the underlying resources cannot be allocated.
+     * @throws InterruptedException If the request was interrupted before retreiving the http response.
+     * @throws ExecutionException If the request completed exceptionally.
+     */
     public List<HashMap<String, String>> getLogEntryList() throws URISyntaxException, IOException, InterruptedException, ExecutionException {
         return this.getLogEntryList(new ListBuilder());
     }
 
+    /**
+     * Get a list of log entries from the server.
+     * @param builder The query string builder to generate a query string for filtering and sorting.
+     * @return A list of log entries from the server represented as a hash map.
+     * @throws URISyntaxException If the query entries ruin the query string syntax.
+     * @throws IOException If the underlying resources cannot be allocated.
+     * @throws InterruptedException If the request was interrupted before retreiving the http response.
+     * @throws ExecutionException If the request completed exceptionally.
+     */
     public List<HashMap<String, String>> getLogEntryList(ListBuilder builder) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
 
         String queryString = "?";
@@ -99,7 +131,14 @@ public class LogClient{
 
     }
 
-    private CompletableFuture<HttpResponse<String>> getAsync(String endpoint) throws URISyntaxException, IOException, InterruptedException {
+    /**
+     * Elementary asynchronous get request.
+     * @param endpoint Where to send the request to.
+     * @return The Http response promise.
+     * @throws URISyntaxException If the URI syntax is incorrect.
+     * @throws IOException If the underlying resources cannot be allocated.
+     */
+    private CompletableFuture<HttpResponse<String>> getAsync(String endpoint) throws URISyntaxException, IOException {
 
         HttpClient client = HttpClient.newBuilder()
             .build();
@@ -107,7 +146,7 @@ public class LogClient{
         HttpRequest request = HttpRequest.newBuilder()
             .GET()
             .uri(
-                new URI(this.url+":"+this.port+"/"+endpoint)
+                new URI(this.url+":"+this.port+endpoint)
             )
             .build();
 
@@ -115,14 +154,30 @@ public class LogClient{
 
     }
 
+    /**
+     * Elementary synchronous get request.
+     * @param endpoint Where to send the request to.
+     * @return The Http response.
+     * @throws URISyntaxException If the URI syntax is incorrect.
+     * @throws IOException If the underlying resources cannot be allocated.
+     * @throws InterruptedException If the underlying asynchronous request was interrupted before retreival.
+     * @throws ExecutionException If the underlying asynchronous request completed exceptionally.
+     */
     private HttpResponse<String> get(String endpoint) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
 
         return this.getAsync(endpoint).get();
 
     }
 
-
-    private CompletableFuture<HttpResponse<String>> postAsync(String endpoint, String payload) throws URISyntaxException, IOException, InterruptedException {
+    /**
+     * Elementary asynchronous post request.
+     * @param endpoint Where to send the requests to.
+     * @param payload Where to send to the server.
+     * @return The Http response promise.
+     * @throws URISyntaxException If the URI syntax is incorrect.
+     * @throws IOException If the underlying resources cannot be allocated.
+     */
+    private CompletableFuture<HttpResponse<String>> postAsync(String endpoint, String payload) throws URISyntaxException, IOException {
 
         HttpClient client = HttpClient.newBuilder()
             .build();
@@ -130,7 +185,7 @@ public class LogClient{
         HttpRequest request = HttpRequest.newBuilder()
             .POST(HttpRequest.BodyPublishers.ofString(payload))
             .uri(
-                new URI(this.url+endpoint)
+                new URI(this.url+":"+this.port+endpoint)
             )
             .build();
 
@@ -138,6 +193,16 @@ public class LogClient{
 
     }
 
+    /***
+     * Elementary synchronous post request.
+     * @param endpoint Where to send the request to.
+     * @param payload What to send to the server.
+     * @return  The Http response.
+     * @throws URISyntaxException If the URI syntax is incorrect.
+     * @throws IOException If the underlying resources cannot be allocated.
+     * @throws InterruptedException If the underlying asynchronous request was interrupted before retreival.
+     * @throws ExecutionException If the underlying asynchronous request completed exceptionally.
+     */
     private HttpResponse<String> post(String endpoint, String payload) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
 
         return this.postAsync(endpoint, payload).get();
@@ -145,50 +210,101 @@ public class LogClient{
     }
 
 
-
+    /**
+     * Builder for a LogClient.
+     * @see LogClient
+    */
     public static class LogClientBuilder {
 
+        /** The url to send the requests to */
         private String url;
+
+        /** The port to send the requests to*/
         private int port;
 
+        /**
+         * Set the url to the server.
+         * @param server_url
+         */
         public void url(String server_url) {
             this.url = server_url;
         }
 
+        /**
+         * Set the port to the server.
+         * @param server_prot
+         */
         public void port(int server_prot) {
             this.port = server_prot;
         }
 
+        /**
+         * Build a LogClient.
+         * @return the LogClient.
+         */
         public LogClient build() {
             return new LogClient(this);
         }
 
     }
 
+    /**
+     * Builder for filtering and sorting the list of log entries.
+     * @see #getLogEntryList
+     */
     public static class ListBuilder {
 
+        /** Wether the sorting should be reversed. */
         private Boolean reverse = null;
+
+        /** How the list should be sorted. */
         private String sort = null;
+
+        /** What category should be filtered for. */
         private String category = null;
+
+        /** What subcategory should be filtered for. */
         private String subCategory = null;
+
+        /** What date interval should be included */
         private String date = null;
 
+        /**
+         * Set reverse.
+         * @param reverse
+         */
         public void reverse(boolean reverse) {
             this.reverse = reverse;
         }
 
+        /**
+         * Set sorting.
+         * @param sort
+         */
         public void sort(String sort) {
             this.sort = sort;
         }
 
+        /**
+         * Set category filtering.
+         * @param category
+         */
         public void category(String category) {
             this.category = category;
         }
 
+        /**
+         * Set subCategory filtering.
+         * @param subCategory
+         */
         public void subCategory(String subCategory) {
             this.subCategory = subCategory;
         }
 
+        /**
+         * Set date filterin.
+         * @param date
+         */
         public void date(String date) {
             this.date = date;
         }
