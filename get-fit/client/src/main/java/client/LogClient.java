a7508f3d1e76b10104eee39de473755c2ead396e
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2021-11-10 16:39:02 +0100

    Fix unused import client

--- get-fit/client/src/main/java/client/LogClient.java
+++ get-fit/client/src/main/java/client/LogClient.java
@@ -12,7 +12,6 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
