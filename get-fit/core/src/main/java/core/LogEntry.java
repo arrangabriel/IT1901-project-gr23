998ba1004d5b4fe7b3331fcb9be2dd1e026db5ea
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-11-15 15:18:14 +0000

    Move validity to separate file

--- get-fit/core/src/main/java/core/LogEntry.java
+++ get-fit/core/src/main/java/core/LogEntry.java
@@ -543,50 +543,7 @@ public final class LogEntry {
 
     // #endregion getters
 
-    /**
-     * Validity of a EntryBuilder.
-     */
-    public static class Validity {
-
-        /**
-         * Whether the builder is valid.
-         */
-        private final boolean isValid;
-
-        /**
-         * The reason for the validity of the builder.
-         */
-        private final String validityReason;
-
-        /**
-         * Object representing the validity of an EntryBuilder.
-         *
-         * @param valid  whether the builder is valid.
-         * @param reason "ok" if valid, otherwise the reason the builder is not valid.
-         */
-        public Validity(final boolean valid, final String reason) {
-            this.isValid = valid;
-            this.validityReason = reason;
-        }
-
-        /**
-         * Whether the builder is valid.
-         *
-         * @return the validity
-         */
-        public boolean invalid() {
-            return !this.isValid;
-        }
-
-        /**
-         * The reason for the validity of the builder.
-         *
-         * @return the reason.
-         */
-        public String reason() {
-            return this.validityReason;
-        }
-    }
+    
 
     /**
      * Builder class for LogEntry.
