d3093caa87d41483be12e2b425bb98f74cd012c6
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-11-20 14:52:10 +0000

    #143 Test addLogEntry

--- get-fit/integration/src/test/java/integration/GetfitIntegrationTest.java
+++ get-fit/integration/src/test/java/integration/GetfitIntegrationTest.java
@@ -1,6 +1,7 @@
 package integration;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import client.LogClient;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -26,10 +28,60 @@ public class GetfitIntegrationTest {
 
     @Autowired
     public GetfitController controller;
+    public LogClient logClient;
+
+    @BeforeEach
+    public void startClient() {
+        this.logClient = new LogClient("http://localhost", 8080);
+    }
 
     @Test
     public void testCompilation() {
         Assertions.assertNotNull(controller);
+    }
+
+    @Test
+    public void testEntryCreation() {
+        HashMap<String, String> entry = new HashMap<>();
+
+        entry.put("title", "Example title");
+        entry.put("content", "Example content");
+        entry.put("date", "2020-01-01");
+        entry.put("feeling", "7");
+        entry.put("duration", "3600");
+        entry.put("distance", "3");
+        entry.put("maxHeartRate", "150");
+        entry.put("exerciseCategory", "STRENGTH");
+        entry.put("exerciseSubCategory", "PULL");
+
+        String id = logClient.addLogEntry(entry);
+    }
+
+    private String createEntry(
+            String title, 
+            String comment,
+            String date,
+            String feeling,
+            String duration,
+            String distance,
+            String maxHeartRate,
+            String exerciseCategory,
+            String exerciseSubCategory
+        ) {
+
+        HashMap<String, String> entry = new HashMap<>();
+
+        entry.put("title", title);
+        entry.put("content", comment);
+        entry.put("date", date);
+        entry.put("feeling", feeling);
+        entry.put("duration", duration);
+        entry.put("distance", distance);
+        entry.put("maxHeartRate", maxHeartRate);
+        entry.put("exerciseCategory", exerciseCategory);
+        entry.put("exerciseSubCategory", exerciseSubCategory);
+
+        return logClient.addLogEntry(entry);
     }
 
 }
