9016129c9e1680c7150beff0799145a701ae89e0
Arran Gabriel <arrankg@gmail.com>
2021-11-21 11:12:58 +0100

    Add logo and icon. Closes #76 Create logo
    Signed-off-by: Arran Gabriel <arrankg@gmail.com>

--- get-fit/ui/src/main/java/ui/App.java
+++ get-fit/ui/src/main/java/ui/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -37,6 +38,7 @@ public class App extends Application {
             stage.setTitle("Get fit");
             stage.setScene(new Scene(parent));
             stage.setResizable(false);
+            stage.getIcons().add(new Image(App.class.getResourceAsStream("images/icon.png")));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
